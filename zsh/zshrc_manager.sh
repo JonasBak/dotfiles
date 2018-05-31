f4716352d8114df141a3284919af19b205148b9b
Jonas <jonasxb@gmail.com>
2018-05-31 11:08:10 +0200

    Remove zsh_manager

--- zsh/zshrc_manager.sh
+++
@@ -1,9 +0,0 @@
-# Run tmux if exists
-if command -v tmux>/dev/null; then
-    [ -z $TMUX ] && exec tmux
-  else 
-    echo "tmux not installed. Run ./deploy to configure dependencies"
-fi
-export OMZ=~/.oh-my-zsh
-export ZAS=~/.zsh-autosuggestions
-source $DOTFILES/zsh/zshrc
