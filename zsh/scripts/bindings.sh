1b5cf8287f036b52364ae4942fef1df36aaedd1f
Jonas Bakken <jonasbak@stud.ntnu.no>
2018-06-01 13:43:08 +0200

    Add support for local scripts

--- zsh/scripts/bindings.sh
+++ zsh/scripts/bindings.sh
@@ -2,6 +2,10 @@ dot(){
   if [[ "$1" = "edit" || "$1" = "e" ]]; then
     cd $DOTFILES
     vim .
+  elif [[ "$1" = "install" || "$1" = "i" ]]; then
+    cd $DOTFILES
+    git pull
+    . ./install
   elif [[ "$1" = "update" || "$1" = "u" ]]; then
     . $DOTFILES/update
   else
