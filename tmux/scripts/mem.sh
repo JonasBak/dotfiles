70825da9c37c535d969e41bf7418a97efec608bb
Jonas Bakken <jonasxb@gmail.com>
2019-05-08 20:37:06 +0200

    Show memory used instead of free

--- tmux/scripts/mem.sh
+++ tmux/scripts/mem.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Mem:$(free -t | \
   grep "Mem" | \
-  awk '{print int($4/$2 * 100)}' | \
+  awk '{print int($3/$2 * 100)}' | \
   awk '$1<=35 {printf "#[fg=colour112]"} \
   $1>30 {printf "#[fg=colour215]"} \
   $1>70 {printf "#[fg=colour160]"} \
