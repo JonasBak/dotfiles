9a6a2bc3ce47d768bbc042ec30636d37f9a0dabf
Jonas Bakken <jonasxb@gmail.com>
2019-05-17 23:47:11 +0200

    Start adding file icons

--- tmux/scripts/docker.sh
+++ tmux/scripts/docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if command -v docker>/dev/null; then
-  echo -e "\uf308 $(docker ps -q | wc -l)"
+  echo -e "#{?PATCHED_FONT,\uf308,🐋} $(docker ps -q | wc -l) |"
 fi
--- tmux/scripts/kubernetes.sh
+++ tmux/scripts/kubernetes.sh
@@ -2,9 +2,9 @@
 if command -v kubectl>/dev/null; then
   config=$(kubectl config current-context)
   if [ -z $config ]; then
-    echo ⎈ [none]
+    echo -e "#{?PATCHED_FONT,\ufd31,⎈} [none] |"
   else
-    echo ⎈ $config
+    echo -e "#{?PATCHED_FONT,\ufd31,⎈} $config |"
   fi
 fi
 
