258b80b69ceb9bc373dcc588ce56d821184ed035
Jonas Bakken <jonasxb@gmail.com>
2019-05-22 18:53:30 +0200

    Fix bug in tmux status bar

--- tmux/scripts/cpu.sh
+++ tmux/scripts/cpu.sh
@@ -8,7 +8,7 @@ echo "CPU:$(top -bn2 | \
   sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
   tail -1 | \
   awk '{print int(100 - $1)}' | \
-  awk '$1<=35 {printf "#[fg=colour112]"} \
+  awk '{printf "#[fg=colour112]"} \
   $1>30 {printf "#[fg=colour215]"} \
   $1>70 {printf "#[fg=colour160]"} \
   {printf "%3d%#[fg=colour15]", $1}')"
