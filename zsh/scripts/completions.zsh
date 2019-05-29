482217116af31c99110c8e91864ae544f8b9d0cd
Jonas Bakken <jonasxb@gmail.com>
2019-05-29 18:27:14 +0200

    Clean up completions

--- zsh/scripts/completions.zsh
+++ zsh/scripts/completions.zsh
@@ -8,3 +8,19 @@ fi
 if [[ ! -f $DOTFILES/local/zsh_completions/_docker ]]; then
   curl -fLo $DOTFILES/local/zsh_completions/_docker https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker
 fi
+
+if [[ -f $HOME/.zcompdump ]]; then
+  last_comp_dump=$(date -r $HOME/.zcompdump +%s)
+  current_time=$( date +%s )
+  autoload -Uz compinit
+  if (( last_comp_dump < (current_time-(60*60*24)) )); then
+    autoload -Uz compdump
+    compinit
+    compdump
+  else
+    compinit -C
+  fi
+else
+  autoload -Uz compinit
+  compinit
+fi
