92cc31094180c1f2542541250f2e4a249a019f9a
Jonas Bakken <jonasxb@gmail.com>
2019-05-31 16:49:04 +0200

    Clean up

--- zsh/scripts/completions.zsh
+++ zsh/scripts/completions.zsh
@@ -2,10 +2,10 @@
 zstyle ':completion:*' menu select
 
 # Only redo completion cache once per day
+autoload -Uz compinit
 if [[ -f $HOME/.zcompdump ]]; then
   last_comp_dump=$(date -r $HOME/.zcompdump +%s)
   current_time=$( date +%s )
-  autoload -Uz compinit
   if (( last_comp_dump < (current_time-(60*60*24)) )); then
     autoload -Uz compdump
     compinit
@@ -14,6 +14,5 @@ if [[ -f $HOME/.zcompdump ]]; then
     compinit -C
   fi
 else
-  autoload -Uz compinit
   compinit
 fi
