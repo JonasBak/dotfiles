5ceb74565b7d9e0da2a5e8b7b5a686e970ae635f
Jonas Bakken <jonasxb@gmail.com>
2019-11-25 12:22:32 +0100

    Fix bar-volume and lightline colors

--- sway/bar.sh
+++ sway/bar.sh
@@ -5,6 +5,6 @@ DATE=$(date +'%d-%m-%Y')
 
 BATTERY=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage/{print $2}')
 
-VOLUME=$(pactl list sinks | awk '/Volume/{print $5}' | head -n 1)
+VOLUME=$(amixer sget Master | grep -oE "[0-9]{1,3}%" | head -n 1)
 
 echo -e "🔉$VOLUME ⚡$BATTERY $TIME $DATE "
